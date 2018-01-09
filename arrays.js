f3f55923378993f9bed644832a8fdae27a9a3b03
Jaclyn Hall <jaclynchall@gmail.com>
2018-01-09 00:32:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+  function addElementToBeginningOfArray (chocolateBars, candyString) {
+    return [candyString, ...chocolateBars]
+  }
+
   function destructivelyAddElementToBeginningOfArray (chocolateBars, candyString) {
     chocolateBars.unshift(candyString)
     return chocolateBars
