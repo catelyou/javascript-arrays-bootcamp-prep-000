5cab89010120b06edbf2dad5ccbe360bf4671ba9
Jaclyn Hall <jaclynchall@gmail.com>
2018-01-09 00:55:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
   function accessElementInArray(array, index) {
     return array[index]
   };
+  
   function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
     chocolatebars.shift()
     return chocolateBars
