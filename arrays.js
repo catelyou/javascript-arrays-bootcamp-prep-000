9cfee88340b5f425df127772bae5c8bdef75ddb3
Jaclyn Hall <jaclynchall@gmail.com>
2018-01-09 00:56:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,26 +2,26 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
   function addElementToBeginningOfArray (chocolateBars, candyString) {
     return [candyString, ...chocolateBars]
-  };
+  }
 
   function destructivelyAddElementToBeginningOfArray (chocolateBars, candyString) {
     chocolateBars.unshift(candyString)
     return chocolateBars
-  };
+  }
 
   function addElementToEndOfArray (chocolateBars, candyString) {
     return [...chocolateBars, candyString]
-  };
+  }
 
   function destructivelyAddElementToEndOfArray (chocolateBars, candyString) {
     chocolateBars.push(candyString)
     return chocolateBars
-  };
+  }
 
   function accessElementInArray(array, index) {
     return array[index]
-  };
-  
+  }
+
   function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
     chocolatebars.shift()
     return chocolateBars
